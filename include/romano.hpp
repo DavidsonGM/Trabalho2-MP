c18a26b40d46a87388be6f8d6049e3e6a52e7ed2
David Mendes <davidgmendes@hotmail.com>
2021-03-31 16:56:08 -0300

    Ultimas modificacoes

--- include/romano.hpp
+++ include/romano.hpp
@@ -1,2 +1,7 @@
 // Copyright 2021 David Mendes
 
+#include <string>
+
+bool EntradaValida(std::string entrada);
+int ConverterAlgarismo(char algarismo);
+int RomanosParaDecimal(std::string entrada);
