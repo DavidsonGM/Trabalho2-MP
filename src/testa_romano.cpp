874dbdef7d9731d6b787f195f58e95868624070e
David Mendes <davidgmendes@hotmail.com>
2021-03-31 12:00:24 -0300

    Caso de teste 4 falhando

--- src/testa_romano.cpp
+++ src/testa_romano.cpp
@@ -9,4 +9,8 @@ TEST_CASE("E algarismo romano", "[romanos]" ) {
     REQUIRE(ConverterAlgarismo('I') == 1);
     REQUIRE(ConverterAlgarismo('V') == 5);
     REQUIRE(ConverterAlgarismo('X') == 10);
+    REQUIRE(ConverterAlgarismo('L') == 50);
+    REQUIRE(ConverterAlgarismo('C') == 100);
+    REQUIRE(ConverterAlgarismo('D') == 500);
+    REQUIRE(ConverterAlgarismo('M') == 1000);
 }
