3a05e4afe6bf08781bfd50ca5e169bf7d9a6c51a
David Mendes <davidgmendes@hotmail.com>
2021-03-31 11:40:18 -0300

    caso de teste 1 falhou

--- src/romano.cpp
+++ src/romano.cpp
@@ -1,2 +1,15 @@
 // Copyright 2021 David Mendes
 
+#include "../include/romano.hpp"
+
+bool EntradaValida(std::string entrada){
+
+}
+
+int ConverterAlgarismo(char entrada){
+
+}
+
+int RomanosParaDecimal(std::string entrada){
+
+}
--- src/testa_romano.cpp
+++ src/testa_romano.cpp
@@ -1,1 +1,10 @@
 // Copyright 2021 David Mendes
+
+
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include "../include/romano.hpp"
+
+TEST_CASE("E algarismo romano", "[romanos]" ) {
+    REQUIRE(ConverterAlgarismo('I') == '1');
+}
